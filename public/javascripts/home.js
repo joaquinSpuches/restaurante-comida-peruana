dcf265b78512bced370e87453da25bfb4b1633dc
joaquinSpuches <spuchesjoaquin@gmail.com>
2021-11-12 11:11:21 -0300

    primer boceto

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,10 +1,38 @@
 const navBar = () => {
     const burger = document.querySelector('.burger')
     const nav = document.querySelector('.nav-ul')
+    const nav1 = document.querySelector('.navA-1')
+    const nav2 = document.querySelector('.navA-2')
+    const nav3 = document.querySelector('.navA-3')
+    const nav4 = document.querySelector('.navA-4')
+    const main = document.querySelector('main')
 
     burger.addEventListener('click', () => {
+        console.log('activado')
         nav.classList.toggle('active')
+        main.classList.toggle('active')
     })
+    nav1.addEventListener('click', () =>{
+        console.log('desactivado')
+        nav.classList.toggle('active')
+        main.classList.toggle('active')
+    })
+    nav2.addEventListener('click', () =>{
+        console.log('desactivado')
+        nav.classList.toggle('active')
+        main.classList.toggle('active')
+    })
+    nav3.addEventListener('click', () =>{
+        console.log('desactivado')
+        nav.classList.toggle('active')
+        main.classList.toggle('active')
+    })
+    nav4.addEventListener('click', () =>{
+        console.log('desactivado')
+        nav.classList.toggle('active')
+        main.classList.toggle('active')
+    })
+    
 }
 
 
